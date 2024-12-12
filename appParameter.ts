7604b7bd5a31e0d943e320907a1181d6cecc4f6b
saito-kengo <68229123+saito-kengo@users.noreply.github.com>
2024-12-12 13:26:20 +0900

    Add files via upload

---
+++ appParameter.ts
@@ -0,0 +1,40 @@
+// ヒール使用時の消費MP
+export const HEAL_NEED_MP = 10;
+// ヒールの回復量倍率
+export const HEAL_RATIO = 1.5;
+
+// 毒攻撃の消費MP
+export const POISON_NEED_MP = 20;
+// 毒のダメージ
+export const POISON_DAMAGE = 10;
+// 毒の継続ターン数
+export const POISON_TRUN_LIMIT = 3;
+
+// クリティカル発生率
+export const CRITICAL_THRESHOLD = 0.2;
+// クリティカル発生時のダメージ倍率
+export const CRITICAL_RATIO = 2;
+// クリティカル発生率補正値（スピード/補正値を加算）
+export const CRITICAL_CORRECTION_VALUE = 1000;
+ 
+// 防御時のダメージ軽減率
+export const GUARD_RATIO = 0.5;
+
+// スキル使用時の消費MP
+export const SKILL_NEED_MP = 15;
+// スキル使用時の与えるダメージ倍率
+export const SKILL_RATIO = -1000000;
+// スキル使用後に受けるダメージ倍率
+export const SKILLED_RECEIVE_DAMAGE_RATIO = 2;
+
+// ダブルアタック使用時の消費MP
+export const DOUBLE_ATTACK_NEED_MP = 30;
+// ダブルアタックのダメージ倍率
+export const DOUBLE_ATTACK_RATIO = 0.4;
+
+// パラメータ合計時の上限
+export const PARAM_MAX = 400;
+
+// 1ターンの時間
+export const BATTLE_PROGRESS_TIME = 3000;
+export const BATTLE_WAIT_TIME = 1000;

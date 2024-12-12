1e08322346b3fa6ce13bd7e474a409cb527a5f43
saito-kengo <68229123+saito-kengo@users.noreply.github.com>
2024-12-12 13:12:57 +0900

    Add files via upload

--- player02.ts
+++ player02.ts
@@ -1,15 +1,14 @@
 import { ACTION_TYPE, ActionType } from "@/constants/type";
 
-export const P2_ACTION_LIST: ActionType[] = [
-]
+export const P2_ACTION_LIST: ActionType[] = [ACTION_TYPE.SKILL,ACTION_TYPE.HEAL,ACTION_TYPE.SKILL,ACTION_TYPE.GUARD]
 
 export const PLAYER02 = {
-    name: "プレイヤー2",
+    name: "ちきばんてぇてぇ",
     hp: 50,
-    mp: 50,
-    str: 50,
-    def: 50,
-    speed: 50,
+    mp: 40,
+    str: 210    ,
+    def: 0,
+    speed: 100,
     actions: P2_ACTION_LIST,
-    image: "player.svg"
+    image: "フリーナ.jpg"
 }

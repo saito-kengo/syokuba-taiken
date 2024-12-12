9dc3a5c33503d93260d50eba064354541aa76fa6
saito-kengo <68229123+saito-kengo@users.noreply.github.com>
2024-12-12 13:37:18 +0900

    Add files via upload

--- player01.ts
+++ player01.ts
@@ -1,7 +1,7 @@
 import { ACTION_TYPE, ActionType } from "@/constants/type";
 export const P1_ACTION_LIST: ActionType[] = [
-    ACTION_TYPE.ATTACK,
-    ACTION_TYPE.ATTACK,
+    ACTION_TYPE.DOUBLE_ATTACK,
+    ACTION_TYPE.POISON,
     ACTION_TYPE.ATTACK,
     ACTION_TYPE.ATTACK,
     ACTION_TYPE.ATTACK,
@@ -29,11 +29,11 @@ export const P1_ACTION_LIST: ActionType[] = [
 
 export const PLAYER01 = {
     name: "tikinnnn",
-    hp: 200,
-    mp: 0,
-    str: 200,
+    hp: 10,
+    mp: 40,
+    str: 50,
     def: 0,
-    speed: 0,
+    speed: 300,
     actions: P1_ACTION_LIST,
     image: "player.svg"
 }
